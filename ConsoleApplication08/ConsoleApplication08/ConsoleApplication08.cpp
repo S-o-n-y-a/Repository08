bdd435da2bb3f901a27a1b230a457df408f9df9a
S-o-n-y-a <99415997+S-o-n-y-a@users.noreply.github.com>
2023-11-08 21:56:18 +0300

    Update ConsoleApplication08.cpp

--- ConsoleApplication08/ConsoleApplication08/ConsoleApplication08.cpp
+++ ConsoleApplication08/ConsoleApplication08/ConsoleApplication08.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "rus");
-
+    //добавлю-ка комментарий-1
     cout << "ДЗ на 09-11-2023";
     cout << endl;
-
+    //добавлю-ка комментарий-2
     cout << "Add smth new";
     cout << endl;
 
